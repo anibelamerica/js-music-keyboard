73a69120a1296d61dfddbf68998e90305d45f54d
Anibel America <ameriabel@gmail.com>
2018-11-19 16:37:50 -0800

    adds trigger on click and on keydown for note playing

--- noise.js
+++ noise.js
@@ -1,3 +1,23 @@
 $(document).ready( function() {
   // your code here
+  $('.instrument').on('click', 'button', function(event) {
+    const clickNote = $(this).html();
+    playNote(clickNote);
+  });
+
+  $('body').keydown((event) => {
+    if (notes.includes(event.key)) {
+      console.log(event.key);
+      const note = event.key;
+      playNote(note);
+    }
+  });
 });
+
+const notes = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+const playNote = (note) => {
+  let target = $(`#${note}Audio`).get(0);
+  target.currentTime = 0;
+  target.play();
+}
